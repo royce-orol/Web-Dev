e5f7cca36634fdc7459d425f665127376cf05472
BryanLim21 <bryanlimj@gmail.com>
2024-12-24 20:27:29 +0800

    logout

--- includes/header.php
+++ includes/header.php
@@ -36,7 +36,7 @@ $role = $_SESSION['role']; // student, moderator, admin, supervisor
             </a>
         </div>
         <div class=logout-button>
-            <a href="logout.php" onclick="return confirm('Are you sure you want to logout?')">
+            <a href="log-out.php" onclick="return confirm('Are you sure you want to logout?')">
             <img src="images/logout.png" title="log-out" />
         </a>
 
